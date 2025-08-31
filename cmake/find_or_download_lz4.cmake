40291a7f2a62561a7c215bc30eff69d54724b274
Davide Faconti <davide.faconti@gmail.com>
2025-08-31 16:06:12 +0200

    fix conan builds

--- cmake/find_or_download_lz4.cmake
+++ cmake/find_or_download_lz4.cmake
@@ -1,37 +1,35 @@
 function(find_or_download_lz4)
 
-  find_package(LZ4 QUIET)
+  # this should default to conan
+  find_package(lz4 QUIET CONFIG)
 
-  if(LZ4_FOUND)
-    message(STATUS "Found LZ4 in system")
+  if(TARGET LZ4::lz4_static)
 
-    if(NOT TARGET lz4::lz4)
-      add_library(lz4::lz4 INTERFACE IMPORTED)
-      set_target_properties(lz4::lz4 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                                "${LZ4_INCLUDE_DIR}")
-      target_link_libraries(lz4::lz4 INTERFACE ${LZ4_LIBRARY})
-    endif()
+    message(STATUS "Found LZ4 using conan")
 
-  elseif(NOT TARGET lz4_static)
+  else()
     # lz4 ###
     cpmaddpackage(
-      NAME lz4 URL https://github.com/lz4/lz4/archive/refs/tags/v1.10.0.zip
+      NAME lz4
+      URL https://github.com/lz4/lz4/archive/refs/tags/v1.10.0.zip
       DOWNLOAD_ONLY YES)
+
     file(GLOB LZ4_SOURCES ${lz4_SOURCE_DIR}/lib/*.c)
     add_library(lz4_static STATIC ${LZ4_SOURCES})
     target_include_directories(lz4_static PUBLIC ${lz4_SOURCE_DIR}/lib)
 
     set(LZ4_FOUND TRUE)
 
-    add_library(lz4::lz4 INTERFACE IMPORTED)
+    add_library(LZ4::lz4_static INTERFACE IMPORTED)
 
     set_target_properties(
-      lz4::lz4 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${lz4_SOURCE_DIR}/lib
-                          INTERFACE_LINK_LIBRARIES lz4_static)
+      LZ4::lz4_static
+      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${lz4_SOURCE_DIR}/lib
+                 INTERFACE_LINK_LIBRARIES lz4_static)
 
   endif()
 
-  if(NOT TARGET lz4::lz4)
+  if(NOT TARGET LZ4::lz4_static)
     message(FATAL_ERROR "LZ4 not found, please install LZ4 or download it")
   endif()
 
--- cmake/find_or_download_zstd.cmake
+++ cmake/find_or_download_zstd.cmake
@@ -1,76 +1,42 @@
 function(find_or_download_zstd)
 
-  find_package(ZSTD QUIET)
+  # this should default to conan
+  find_package(zstd QUIET CONFIG)
 
-  if(ZSTD_FOUND)
-    message(STATUS "Found ZSTD in system")
-
-    if(NOT TARGET zstd::zstd)
-      add_library(zstd::zstd INTERFACE IMPORTED)
-      set_target_properties(zstd::zstd PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                                  "${ZSTD_INCLUDE_DIR}")
-      target_link_libraries(zstd::zstd INTERFACE ${ZSTD_LIBRARY})
-    endif()
-
-  elseif(NOT TARGET libzstd_static)
+  if(TARGET zstd::libzstd_static)
+    message(STATUS "Found ZSTD in system (conan?)")
+  else()
     # zstd ###
     cpmaddpackage(
-      NAME zstd URL
-      https://github.com/facebook/zstd/archive/refs/tags/v1.5.7.zip
+      NAME zstd
+      URL https://github.com/facebook/zstd/archive/refs/tags/v1.5.7.zip
       DOWNLOAD_ONLY YES)
-    set(ZSTD_BUILD_STATIC
-        ON
-        CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_SHARED
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_LEGACY_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_PROGRAMS
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_TESTS
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_CONTRIB
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_EXAMPLES
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_MULTITHREAD_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_LEGACY_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_ZLIB_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_LZ4_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_LZMA_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_ZDICT_SUPPORT
-        OFF
-        CACHE BOOL " " FORCE)
-    set(ZSTD_PROGRAMS
-        ""
-        CACHE STRING " " FORCE)
+
+    set(ZSTD_BUILD_STATIC ON CACHE BOOL " " FORCE)
+    set(ZSTD_BUILD_SHARED OFF CACHE BOOL " " FORCE)
+    set(ZSTD_LEGACY_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_BUILD_PROGRAMS OFF CACHE BOOL " " FORCE)
+    set(ZSTD_BUILD_TESTS OFF CACHE BOOL " " FORCE)
+    set(ZSTD_BUILD_CONTRIB OFF CACHE BOOL " " FORCE)
+    set(ZSTD_BUILD_EXAMPLES OFF CACHE BOOL " " FORCE)
+    set(ZSTD_MULTITHREAD_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_LEGACY_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_ZLIB_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_LZ4_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_LZMA_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_ZDICT_SUPPORT OFF CACHE BOOL " " FORCE)
+    set(ZSTD_PROGRAMS "" CACHE STRING " " FORCE)
 
     add_subdirectory(${zstd_SOURCE_DIR}/build/cmake ${zstd_BINARY_DIR})
     set(ZSTD_FOUND TRUE)
 
-    add_library(zstd::zstd INTERFACE IMPORTED)
-    set_target_properties(
-      zstd::zstd PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${zstd_SOURCE_DIR}/lib
-                            INTERFACE_LINK_LIBRARIES libzstd_static)
+    add_library(zstd::libzstd_static INTERFACE IMPORTED)
+    set_target_properties(zstd::libzstd_static PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${zstd_SOURCE_DIR}/lib
+        INTERFACE_LINK_LIBRARIES libzstd_static)
   endif()
 
-  if(NOT TARGET zstd::zstd)
+  if(NOT TARGET zstd::libzstd_static)
     message(FATAL_ERROR "ZSTD not found, please install ZSTD or download it")
   endif()
 
